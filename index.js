822fd8377a39447144b30f11cd9e9da91d695d80
BenjaminJMcD <BenjaminJMcD@outlook.com>
2024-03-21 12:44:28 -0500

    webpack created for bst module. class Node edited for coordinate tree

---
+++ index.js
@@ -0,0 +1,15 @@
+import { Node } from './bts';
+import { Tree } from './bts';
+
+
+// how so i create a binary search tree with 2 digit coords. 
+
+
+
+
+
+
+
+function knightMoves(coord1, coord2) {
+    
+};
