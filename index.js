23c1924f8c1568d356bbdad4a8b7f3fc769e6f85
BenjaminJMcD <BenjaminJMcD@outlook.com>
2024-03-26 17:43:24 -0500

    knightMoves returned path - needs to be checked if correct

--- index.js
+++ index.js
@@ -1,18 +1,11 @@
 import { convertToCoord } from "./bts";
 import { convertToMatrix } from "./bts";
 import { createAdjList } from "./bts";
+import { knightMoves } from "./bts";
 
-// how so i create a binary search tree with 2 digit coords. 
-
-
-
-console.log(convertToMatrix([1,2]));
-console.log(convertToCoord(17));
 
 console.log(createAdjList());
 
-console.log(15%8)
 
-function knightMoves(coord1, coord2) {
-    
-};
+let shortestPath = knightMoves([7,7], [2,4]);
+console.log(shortestPath)
