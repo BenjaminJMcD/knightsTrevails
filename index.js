86ecadd995b5a4508d282b2e3292084dc43798c2
BenjaminJMcD <BenjaminJMcD@outlook.com>
2024-03-27 12:06:44 -0500

    Function works. Added descript comments and result to console

--- index.js
+++ index.js
@@ -1,11 +1,4 @@
-import { convertToCoord } from "./bts";
-import { convertToMatrix } from "./bts";
-import { createAdjList } from "./bts";
 import { knightMoves } from "./bts";
 
 
-console.log(createAdjList());
-
-
-let shortestPath = knightMoves([7,7], [2,4]);
-console.log(shortestPath)
+knightMoves([0,0], [6,1]);
